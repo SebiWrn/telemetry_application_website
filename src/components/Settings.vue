7248bc4e3111193f4b96ef3066efa745d96f4acf
Sebastian <TheUniquePro@users.noreply.github.com>
2022-10-20 10:05:17 +0200

    Added settings config file and folder for config files. Also made list of settings to match json file

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,20 +1,52 @@
 <template>
   <div class="settings">
+    <div class="container">
+      <ul>
+        <li v-for="setting in settings" :key="setting">
+          <span class="setting">{{setting}}</span>
+          <input
+            type="checkbox"
+            class="setting-checkbox"
+            name=""
+            id="" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import settingsImp from '@/config/settings.json';
 
 export default {
   name: "SettingsView",
-  components: {
-  },
+  components: {},
   data() {
-    return {
+    return {};
+  },
+  computed: {
+    settings() {
+      return settingsImp.settings;
     }
+  },
+  mounted() {
   }
 };
 </script>
 
 <style lang="scss" scoped>
+li {
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: 90% 10%;
+}
+
+.container {
+  padding: calc((1vw + 1vh) / 2);
+}
+
+.setting {
+  color: $text_color;
+  text-align: left;
+}
 </style>
